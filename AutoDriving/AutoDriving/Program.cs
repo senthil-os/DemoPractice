33a21dfc1b58fbf26ab1200180c5f2a1d5dc76e1
senthil-os <156709911+senthil-os@users.noreply.github.com>
2024-01-16 08:03:50 +0530

    Update Program.cs
    Added code comments

--- AutoDriving/AutoDriving/Program.cs
+++ AutoDriving/AutoDriving/Program.cs
@@ -7,15 +7,18 @@ namespace AutoDriving
     {
      public static void Main(string[] args)
         {
+            //Inputs
             var inputMaxCoordinate = Console.ReadLine().Trim();
             var inputStartPosition = Console.ReadLine().Trim().ToUpper();
             var destinationPath  = Console.ReadLine().Trim().ToUpper();
 
             AutoDriving.Util.Util util = new Util.Util();
+
+            //Validate
             var maxCoordinate = util.ValidateMaxCoordinate(inputMaxCoordinate);
             var startPosition = util.ValidateStartPosition(inputStartPosition);
             bool isPathValid = util.ValidateDestinationPath(destinationPath);
-
+             // Valid inputs then execute 
             if (maxCoordinate.Item3 && startPosition.Item4 && isPathValid)
             {
                 VehicleMovement vehicle = new VehicleMovement();
@@ -24,12 +27,19 @@ namespace AutoDriving
                 vehicle.MaxX = maxCoordinate.Item1;
                 vehicle.MaxY = maxCoordinate.Item2;
 
+                //start position should not lesser than Min coordinates
+                vehicle.PositionX = startPosition.Item1 < vehicle.MinX ? vehicle.MinX : startPosition.Item1;
+                vehicle.PositionY = startPosition.Item2 < vehicle.MinY ? vehicle.MinY : startPosition.Item2;
                 //start position should not greater than Max coordinates
                 vehicle.PositionX = startPosition.Item1 > vehicle.MaxX ? vehicle.MaxX : startPosition.Item1;
                 vehicle.PositionY = startPosition.Item2 > vehicle.MaxY ? vehicle.MaxY : startPosition.Item2;
 
                 vehicle.Direction = (AutoDriving.Util.Direction)startPosition.Item3;
+                
+                //Vehicle Journey starts
                 var currentPosition = vehicle.JourneyDestination(destinationPath);
+
+                //Vehicle Destination reached
                 Console.WriteLine("{0} {1} {2}", currentPosition.Item1, currentPosition.Item2, currentPosition.Item3);
             }
             Console.ReadKey();
