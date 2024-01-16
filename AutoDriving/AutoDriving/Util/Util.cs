fd5f8a8554fc725c2b4166022f21ae16fc41a144
senthil-os <156709911+senthil-os@users.noreply.github.com>
2024-01-16 08:06:37 +0530

    Update Util.cs
    Added code comments

--- AutoDriving/AutoDriving/Util/Util.cs
+++ AutoDriving/AutoDriving/Util/Util.cs
@@ -8,6 +8,11 @@ namespace AutoDriving.Util
     {
         Char splitChar = ' ';
         public Util(){ }
+        /// <summary>
+        /// Validate the maxcoodinate string 
+        /// </summary>
+        /// <param name="strMaxCoordinate"></param>
+        /// <returns></returns>
         public Tuple<int,int,bool> ValidateMaxCoordinate(string strMaxCoordinate)
         {   
             int iMaxX = 0;
@@ -23,6 +28,11 @@ namespace AutoDriving.Util
             catch (Exception ex) { isValid = false; } 
             return Tuple.Create(iMaxX-1 < 0?0: iMaxX - 1, iMaxY-1<0?0: iMaxY - 1, isValid);
         }
+        /// <summary>
+        /// Validate the start position coodinate string 
+        /// </summary>
+        /// <param name="startPosition"></param>
+        /// <returns></returns>
         public Tuple<int,int,char,bool> ValidateStartPosition(string startPosition)
         {   int iPositionX = 0;
             int iPositionY = 0;
@@ -42,6 +52,11 @@ namespace AutoDriving.Util
             }
             return Tuple.Create(iPositionX, iPositionY, direction,isValid);
         }
+        /// <summary>
+        /// Validate the path
+        /// </summary>
+        /// <param name="destinationPath"></param>
+        /// <returns></returns>
         public bool ValidateDestinationPath(string destinationPath)
         {
             char[] path = destinationPath.ToCharArray();
